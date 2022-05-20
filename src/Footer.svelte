ece40fc0ba5dea62d53598ae721fab4f22092263
Alexander Wunschik <alex@wunschik.net>
2022-05-21 00:25:48 +0200

    add imprint to footer

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,13 +1,32 @@
-<span id="footer"> footer </span>
+<span id="footer">
+	<ul>
+		<li><a href="https://fullstax.de/impressum.html">imprint</a></li>
+	</ul>
+</span>
 
 <style>
 	#footer {
 		display: flex;
 		height: 100%;
 		width: 100%;
-		box-sizing: border-box;
 
 		background-color: var(--background);
 		border-top: 1px solid var(--border);
+	}
+
+	ul {
+		list-style: none;
+		margin: auto;
+		padding: 0;
+
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-end;
+		width: 100%;
+	}
+
+	ul li {
+		margin: 0 1em;
+		padding: 0;
 	}
 </style>
