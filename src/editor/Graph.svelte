e682d63a4f37135014e221af4773fffdcbe5e1a4
Alexander Wunschik <alex@wunschik.net>
2022-05-23 23:30:59 +0200

    fix(#5): fix node-selection bug

--- src/editor/Graph.svelte
+++ src/editor/Graph.svelte
@@ -6,6 +6,81 @@
 
 	export let selectedNode;
 
+	function appendNetworkEvents() {
+		networkGraph.on('selectNode', function (params) {
+			console.log(`[Graph] selectNode Event:`, params);
+			selectedNode = networkStore.nodes.get(params.nodes[0]);
+		});
+		networkGraph.on('dragStart', function (params) {
+			console.log(`[Graph] dragStart Event:`, params);
+			/* Only set selectedNode if nodes is valid.
+			 * This fixes #5 */
+			if (params.nodes && params.nodes.length) {
+				selectedNode = networkStore.nodes.get(params.nodes[0]);
+			}
+		});
+		networkGraph.on('deselectNode', function (params) {
+			console.log(`[Graph] deselectNode Event:`, params);
+			selectedNode = null;
+		});
+
+		// disable physics after initialization
+		// TODO: this is not working properly
+		// networkGraph.on('stabilized', params => {
+		// 	if (params.iterations > 1) {
+		// 		console.log(`[Graph] stabilized Event:`, params);
+		// 		networkGraph.setOptions({
+		// 			...options,
+		// 			...{
+		// 				physics: false,
+		// 			},
+		// 		});
+		// 	}
+		// });
+
+		// This is only for Debugging
+		{
+			// [
+			// 	'click',
+			// 	'doubleClick',
+			// 	'oncontext',
+			// 	// 'hold',
+			// 	// 'release',
+			// 	'select',
+			// 	'selectNode',
+			// 	'selectEdge',
+			// 	'deselectNode',
+			// 	'deselectEdge',
+			// 	// 'dragStart',
+			// 	// 'dragging',
+			// 	// 'dragEnd',
+			// 	'controlNodeDragging',
+			// 	'controlNodeDragEnd',
+			// 	// 'hoverNode',
+			// 	// 'blurNode',
+			// 	// 'hoverEdge',
+			// 	// 'blurEdge',
+			// 	// 'zoom',
+			// 	'showPopup',
+			// 	'hidePopup',
+			// 	// 'startStabilizing',
+			// 	// 'stabilizationProgress',
+			// 	// 'stabilizationIterationsDone',
+			// 	// 'stabilized',
+			// 	// 'resize',
+			// 	// 'initRedraw',
+			// 	// 'beforeDrawing',
+			// 	// 'afterDrawing',
+			// 	'animationFinished',
+			// 	'configChange',
+			// ].forEach(event => {
+			// 	networkGraph.on(event, params => {
+			// 		console.log(`[Graph] event "${event}":`, params);
+			// 	});
+			// });
+		}
+	}
+
 	function initNetwork(domElementId) {
 		// create a network
 		const container = document.getElementById(domElementId);
@@ -61,73 +136,11 @@
 					//iterations: 100,
 				},
 			},
+			selectedNode,
 		};
 		networkGraph = new Network(container, data, options);
 
-		networkGraph.on('selectNode', function (params) {
-			console.log(`[Graph] selectNode Event:`, params);
-			selectedNode = networkStore.nodes.get(params.nodes[0]);
-		});
-		networkGraph.on('dragStart', function (params) {
-			console.log(`[Graph] dragStart Event:`, params);
-			selectedNode = networkStore.nodes.get(params.nodes[0]);
-		});
-		networkGraph.on('deselectNode', function (params) {
-			console.log(`[Graph] deselectNode Event:`, params);
-			selectedNode = undefined;
-		});
-
-		// disable physics after initialization
-		networkGraph.on('stabilized', params => {
-			if (params.iterations > 1) {
-				console.log(`[Graph] stabilized Event:`, params);
-				networkGraph.setOptions({
-					...options,
-					...{
-						physics: false,
-					},
-				});
-			}
-		});
-
-		// [
-		// 	'click',
-		// 	'doubleClick',
-		// 	'oncontext',
-		// 	// 'hold',
-		// 	// 'release',
-		// 	'select',
-		// 	'selectNode',
-		// 	'selectEdge',
-		// 	'deselectNode',
-		// 	'deselectEdge',
-		// 	// 'dragStart',
-		// 	// 'dragging',
-		// 	// 'dragEnd',
-		// 	'controlNodeDragging',
-		// 	'controlNodeDragEnd',
-		// 	// 'hoverNode',
-		// 	// 'blurNode',
-		// 	// 'hoverEdge',
-		// 	// 'blurEdge',
-		// 	// 'zoom',
-		// 	'showPopup',
-		// 	'hidePopup',
-		// 	// 'startStabilizing',
-		// 	// 'stabilizationProgress',
-		// 	// 'stabilizationIterationsDone',
-		// 	// 'stabilized',
-		// 	// 'resize',
-		// 	// 'initRedraw',
-		// 	// 'beforeDrawing',
-		// 	// 'afterDrawing',
-		// 	'animationFinished',
-		// 	'configChange',
-		// ].forEach(event => {
-		// 	networkGraph.on(event, params => {
-		// 		console.log(`[Graph] event "${event}":`, params);
-		// 	});
-		// });
+		appendNetworkEvents();
 
 		return networkGraph;
 	}
@@ -138,11 +151,12 @@
 	});
 </script>
 
-<div id="network" />
-
 <svelte:head>
+	<!-- TODO: we should find a better way to load vis's css! -->
 	<link rel="stylesheet" href="//unpkg.com/vis-network@9.1.2/styles/vis-network.css" />
 </svelte:head>
+
+<div id="network" />
 
 <style>
 	#network {
